68b161a7bc810257a5a2103fbb85fbe1a8203316
Ignacio <ignacio.senese@abx.com>
2025-02-07 10:22:38 +1030

    rendering usequery, adding basequery and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import "./App.css";
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
+import { useQuery } from '@tanstack/react-query'
+import { BASE_URL } from './api'
+import './App.css'
 
 function App() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetch(BASE_URL).then((res) => res.json())
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return `An error has occurred: ${error.message}`
+
   return (
     <>
+      {data.message}
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1 className="text-3xl text-red-800 font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl text-red-800 font-bold underline">Hello world!</h1>
       <Button className="btn-secondary">Click me</Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
