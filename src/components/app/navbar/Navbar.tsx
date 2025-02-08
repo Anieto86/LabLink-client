1e1ac541f3efd887d7dcf7b1a4a6a223c6e9f6fb
Ignacio <ignacio.senese@abx.com>
2025-02-08 16:07:09 +1030

    wip

---
+++ src/components/app/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Row } from "@/components/design/Grid";
+
+const NavBar = () => {
+  return (
+    <Row width="100%" justifyContent="flex-end">
+      {/* <LogoutButton /> */}
+    </Row>
+  );
+};
+
+export default NavBar;
---
+++ src/components/guest/guestNavBar/GuestNavBar.tsx
@@ -0,0 +1,11 @@
+import { Row } from "@/components/design/Grid"
+
+const GuestNavBar = () => {
+  return (
+    <Row className="w-full min-h-10 bg-blue-500 items-center justify-center">
+      GuestNavbar
+    </Row>
+  )
+}
+
+export default GuestNavBar
