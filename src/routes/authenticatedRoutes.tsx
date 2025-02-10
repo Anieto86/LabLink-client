62c67823ecea2384154f92aa4fbc4ba55ad1e3a4
Ignacio <ignacio.senese@abx.com>
2025-02-10 11:47:32 +1030

    fixing auth routes

--- src/routes/authenticatedRoutes.tsx
+++ src/routes/authenticatedRoutes.tsx
@@ -1,15 +1,16 @@
-import AuthenticatedLayout from "@/components/app/AuthenticatedLayout";
-import { lazy } from "react";
-import { Route } from "react-router-dom"; // ✅ Use `react-router-dom`
+import AuthenticatedLayout from '@/components/app/AuthenticatedLayout'
+import { lazy } from 'react'
+import { Navigate, Route } from 'react-router-dom' // ✅ Use `react-router-dom`
 
-const Dashboard = lazy(() => import("@/components/app/dashboard/Dashboard"));
+const Dashboard = lazy(() => import('@/components/app/dashboard/Dashboard'))
 // const Profile = lazy(() => import("@/components/app/Profile"));
 // const Settings = lazy(() => import("@/components/app/Settings"));
 
 export const authenticatedRoutes = (
   <Route element={<AuthenticatedLayout />}>
+    <Route index element={<Navigate to="/dashboard" replace/>} />
     <Route path="dashboard" element={<Dashboard />} />
     {/* <Route path="profile" element={<Profile />} />
     <Route path="settings" element={<Settings />} /> */}
   </Route>
-);
+)
