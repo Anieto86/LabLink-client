d49080f3f4984dbd0a4bba8f14ee7e09a161583c
Ignacio <ignacio.senese@abx.com>
2025-02-10 12:16:16 +1030

    Adding profile and settings pages

--- src/routes/authenticatedRoutes.tsx
+++ src/routes/authenticatedRoutes.tsx
@@ -3,14 +3,14 @@ import { lazy } from 'react'
 import { Navigate, Route } from 'react-router-dom' // ✅ Use `react-router-dom`
 
 const Dashboard = lazy(() => import('@/components/app/dashboard/Dashboard'))
-// const Profile = lazy(() => import("@/components/app/Profile"));
-// const Settings = lazy(() => import("@/components/app/Settings"));
+const Profile = lazy(() => import('@/components/app/profile/Profile'))
+const Settings = lazy(() => import('@/components/app/settings/Settings'))
 
 export const authenticatedRoutes = (
   <Route element={<AuthenticatedLayout />}>
-    <Route index element={<Navigate to="/dashboard" replace/>} />
+    <Route index element={<Navigate to="/dashboard" replace />} />
     <Route path="dashboard" element={<Dashboard />} />
-    {/* <Route path="profile" element={<Profile />} />
-    <Route path="settings" element={<Settings />} /> */}
+    <Route path="profile" element={<Profile />} />
+    <Route path="settings" element={<Settings />} />
   </Route>
 )
