2a3faaeddea1598cd289bde6261fcc29e5245930
Ignacio <ignacio.senese@abx.com>
2025-02-10 12:11:17 +1030

    updating colors

--- src/components/app/sidebar/Sidebar.tsx
+++ src/components/app/sidebar/Sidebar.tsx
@@ -2,20 +2,20 @@ import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <aside className="w-64 h-full bg-gray-900 text-white p-4 fixed left-0 top-0">
+    <aside className="w-48 h-full bg-[#fafafa] dark:bg-[#0a0e1a] p-4 fixed left-0 top-0">
       <ul>
         <li className="mb-4">
-          <Link to="/dashboard" className="text-white hover:underline">
+          <Link to="/dashboard" className="hover:underline">
             Dashboard
           </Link>
         </li>
         <li className="mb-4">
-          <Link to="/profile" className="text-white hover:underline">
+          <Link to="/profile" className=" hover:underline">
             Profile
           </Link>
         </li>
         <li className="mb-4">
-          <Link to="/settings" className="text-white hover:underline">
+          <Link to="/settings" className=" hover:underline">
             Settings
           </Link>
         </li>
