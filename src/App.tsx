0a61645539551ed8083736e4cf19e2ba68ddc0b1
Anieto86 <alvaro16nieto@gmail.com>
2025-03-10 15:37:51 +1030

    Add react-icons package and update Sidebar component to include icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/routes/router'
 import { ErrorBoundary } from 'react-error-boundary'
--- src/app/layout/AuthenticatedLayout.tsx
+++ src/app/layout/AuthenticatedLayout.tsx
@@ -1,19 +1,23 @@
 import { Outlet } from 'react-router-dom'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import Navbar from '@/app/features/navbar/Navbar'
 import Sidebar from '@/app/layout/Sidebar'
 import './Dashboard.css'
 
 const AuthenticatedLayout = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false)
+
   return (
     <div className="grid-areas-dashboard">
       <div className="grid-in-navbar">
         <Navbar />
       </div>
-      <div className="grid-in-sidebar">
+
+      <div className={`grid-in-sidebar ${isSidebarOpen ? 'sidebar-open' : ''}`}>
         <Sidebar />
       </div>
-      <div className="grid-in-main">
+
+      <div className="grid-in-main" onClick={() => setSidebarOpen(false)}>
         <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
         </Suspense>
